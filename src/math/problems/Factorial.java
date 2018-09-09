447659fafbcfe956fd8b84c2a2926f95e3d3672f
upalma <palma.ub13@gmail.com>
2018-09-09 18:04:55 -0400

    MissingNumber

--- src/math/problems/Factorial.java
+++ src/math/problems/Factorial.java
@@ -11,6 +11,16 @@ public class Factorial {
          * Write a java program to find Factorial of a given number using Recursion as well as Iteration.
          *
          */
+        int factorialCount = 5;
+        int[] feb = new int[factorialCount];
+        feb[0] = 0;
+        feb[1] = 1;
+        for (int i = 2; i < factorialCount; i++) {
+            feb[i] = feb[i - 1] + feb[i - 2];
+        }
 
+        for (int i = 0; i < factorialCount; i++) {
+            System.out.print(feb[i] + " ");
+        }
     }
 }
--- src/math/problems/FindMissingNumber.java
+++ src/math/problems/FindMissingNumber.java
@@ -4,14 +4,26 @@ package math.problems;
  * Created by mrahman on 04/22/17.
  */
 public class FindMissingNumber {
-
     public static void main(String[] args) {
+
         /*
          * If n = 10, then array will have 9 elements in the range from 1 to 10.
          * For example {10, 2, 1, 4, 5, 3, 7, 8, 6}. One number will be missing in array (9 in this case).
          * Write java code to find the missing number from the array. Write static helper method to find it.
          */
-         int [] array = new int[]{10, 2, 1, 4, 5, 3, 7, 8, 6};
 
+            int array [] = {10, 2, 9, 4, 5, 3, 1, 8, 6};
+            int missingNo = getMissingNo(array, 9);
+            System.out.println(missingNo);
+        }
+
+        // Function to identifify missing number
+        static int getMissingNo ( int m[], int n){
+            int i, total;
+            total = (n + 1) * (n + 2) / 2;
+            for (i = 0; i < n; i++)
+                total -= m[i];
+            return total;
+        }
     }
-}
+
