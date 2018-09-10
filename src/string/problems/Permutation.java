4be58e69218b836ba587d29264f6574ca3339fd4
upalma <palma.ub13@gmail.com>
2018-09-10 01:26:02 -0400

    permutation

--- src/string/problems/Permutation.java
+++ src/string/problems/Permutation.java
@@ -12,5 +12,38 @@ public class Permutation {
          * Write Java program to compute all Permutation of a String
          *
          */
+        String st = "ABCD";
+        int i = st.length();
+        Permutation permutation = new Permutation();
+        permutation.permute(st, 0, i-1);
     }
+
+    private void permute(String string, int j, int k)
+    {
+        if (j == k)
+            System.out.println(string);
+        else
+        {
+            for (int i = j; i <= k; i++)
+            {
+                string = swap(string,j,i);
+                permute(string, j+1, k);
+                string = swap(string,j,i);
+            }
+        }
+    }
+
+    public String swap(String a, int i, int j)
+    {
+        char temp;
+        char[] charArray = a.toCharArray();
+        temp = charArray[i] ;
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+        return String.valueOf(charArray);
+    }
+
 }
+
+
+
