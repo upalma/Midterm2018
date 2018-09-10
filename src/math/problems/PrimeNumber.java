93ddc6e073a470b414090692fdafe09b8b1384f1
upalma <palma.ub13@gmail.com>
2018-09-09 23:42:25 -0400

    PrimeNumber

--- src/math/problems/PrimeNumber.java
+++ src/math/problems/PrimeNumber.java
@@ -12,7 +12,32 @@ public class PrimeNumber {
 		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
 		 *
 		 */
+		int counter = 0;
+		for(int i=2; i<1000000; i++){
+			if(i==2){
+				System.out.println(i);
+			}else{
+				if(isPrime(i)){
+					counter++;
+					System.out.println(i);
+				}else{
+					System.out.println("Non-Prime: " + i);
+					System.out.println("Total nonPrime numbers: "+(i-counter));
+				}
+			}
+
+		}
+		System.out.println("Total Prime Numbers: " + counter);
 
 	}
 
+	public static boolean isPrime(int n){
+		if(n%2==0)return false;
+		for(int i=3; i*i<=n; i+=2){
+			if(n%i==0) return false;
+		}
+		return true;
+	}
+
 }
+
