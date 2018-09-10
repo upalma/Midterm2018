64de3b25c1cc84a4c43862b0f46150aca25a1188
upalma <palma.ub13@gmail.com>
2018-09-09 20:26:48 -0400

    pattern

--- src/math/problems/Pattern.java
+++ src/math/problems/Pattern.java
@@ -1,5 +1,8 @@
 package math.problems;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pattern {
 
 	public static void main(String[] args) {
@@ -8,8 +11,22 @@ public class Pattern {
 		 *
 		 *
 		 */
-		
 
+		int count = 1, a = 0;
+		int num = 100;
+		List<String> storePattern = new ArrayList<String>();
+		int[] array = new int[40];
 
+		while (num > 0) {
+			for (int i = 0; i < 10; i++) {
+				System.out.print(" " + num);
+				array[a] = num;
+				num -= count;
+				a++;
+
+			}
+			count++;
+		}
+		System.out.println("  ");
 	}
 }
