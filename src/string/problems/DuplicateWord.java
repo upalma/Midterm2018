5f6837c8030e585dd41f84a5c3f0834c6ef73868
upalma <palma.ub13@gmail.com>
2018-09-10 00:11:55 -0400

    DuplicateWord

--- src/string/problems/DuplicateWord.java
+++ src/string/problems/DuplicateWord.java
@@ -1,18 +1,35 @@
 package string.problems;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by mrahman on 04/22/17.
  */
 public class DuplicateWord {
 
-    public static void main(String[] args) {
         /*
          * Write a java program to find the duplicate words and their number of occurrences in the string.
          * Also Find the average length of the words.
          */
-
-        String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
-
-    }
+        public static Set<String> duplicateWords(String input){
+            if(input == null || input.isEmpty()){
+                return Collections.emptySet(); }
+            Set<String> duplicates = new HashSet<>();
+            String[] words = input.split("\\s+");
+            Set<String> set = new HashSet<>();
+            for(String word : words){
+                if(!set.add(word)){
+                    duplicates.add(word); } }
+            return duplicates;
+        }
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        String test = "Java is a programming Language, Java is  computer  a Language. Java is  widely used Language";
+        Set<String> duplicates = duplicateWords(test);
+        System.out.println("input : " + test);
+        System.out.println("output : " + duplicates); }
 
 }
+
