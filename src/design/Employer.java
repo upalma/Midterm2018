a12dd796a236f10cd99381d3933a647096c659b9
upalma <palma.ub13@gmail.com>
2018-09-11 12:48:57 -0400

    updatingdesign

---
+++ src/design/Employer.java
@@ -0,0 +1,9 @@
+package design;
+
+public abstract class Employer {
+    public abstract void employerName();
+
+    public void employeerAddress(){
+        System.out.println("208, 2nd Avenue, Manhatten, NY, 11004");
+    }
+}
