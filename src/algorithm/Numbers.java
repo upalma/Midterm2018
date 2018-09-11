1864ec085fa81a4e0a9ed2e2ca32ae30f338904b
upalma <palma.ub13@gmail.com>
2018-09-11 01:53:12 -0400

    Algorithm Unit Test

--- src/algorithm/Numbers.java
+++ src/algorithm/Numbers.java
@@ -25,111 +25,100 @@ public class Numbers {
 		int [] num = new int[100000];
 		storeRandomNumbers(num);
 		ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
+		int n = num.length;
+		randomize (num, n);
 
 		//Selection Sort
 		Sort algo = new Sort();
 		algo.selectionSort(num);
 		long selectionSortExecutionTime = algo.executionTime;
 		System.out.println("Total Execution Time of "+ num.length + " numbers in Selection Sort take: " + selectionSortExecutionTime + " milli sec");
-        connectToSqlDB.insertDataFromArrayToSqlTable(num, "selection_sort", "SortingNumbers");
-        List<String> numbers = connectToSqlDB.readDataBase("selection_sort", "SortingNumbers");
-        printValue(numbers);
-//		int n = num.length;
-//		randomize (num, n);
+//        connectToSqlDB.insertDataFromArrayToSqlTable(num, "selection_sort", "SortingNumbers");
+//        List<String> numbers = connectToSqlDB.readDataBase("selection_sort", "SortingNumbers");
+//        printValue(numbers);
+
 
 		//Insertion Sort
 		algo.insertionSort(num);
 		long insertionSortExecutionTime = algo.executionTime;
 		System.out.println("Total Execution Time of "+ num.length + " numbers in Insertion Sort take: " + insertionSortExecutionTime + " milli sec");
-		connectToSqlDB.insertDataFromArrayToSqlTable(num, "insertion_sort", "SortingNumbers");
-		List<String> numbers1 = connectToSqlDB.readDataBase("insertion_sort", "SortingNumbers");
-		printValue(numbers);
-		int a = num.length;
-		randomize (num, a);
+//		connectToSqlDB.insertDataFromArrayToSqlTable(num, "insertion_sort", "SortingNumbers");
+//		List<String> numbers1 = connectToSqlDB.readDataBase("insertion_sort", "SortingNumbers");
+//		printValue(numbers);
+
 
 		//Bubble Sort
 		algo.bubbleSort(num);
 		long bubbleSortExecutionTime = algo.executionTime;
 		System.out.println("Total Execution Time of "+ num.length + " numbers in Bubble Sort take: " + bubbleSortExecutionTime + " milli sec");
-		connectToSqlDB.insertDataFromArrayToSqlTable(num, "bubble_sort", "SortingNumbers");
-		List<String> numbers2 = connectToSqlDB.readDataBase("bubble_sort", "SortingNumbers");
-		printValue(numbers);
-		int b = num.length;
-		randomize (num, b);
+//		connectToSqlDB.insertDataFromArrayToSqlTable(num, "bubble_sort", "SortingNumbers");
+//		List<String> numbers2 = connectToSqlDB.readDataBase("bubble_sort", "SortingNumbers");
+//		printValue(numbers);
+//		randomize (num, n);
 
 		//Merge Sort
 		algo.mergeSort(num);
 		long margeSortExecutionTime = algo.executionTime;
 		System.out.println("Total Execution Time of "+ num.length + " numbers in Merge Sort take: " + margeSortExecutionTime+ " milli sec");
-		connectToSqlDB.insertDataFromArrayToSqlTable(num, "merge_sort", "SortingNumbers");
-		List<String> numbers3 = connectToSqlDB.readDataBase("merge_sort", "SortingNumbers");
-		printValue(numbers);
-		int i = num.length;
-		randomize (num, i);
+//		connectToSqlDB.insertDataFromArrayToSqlTable(num, "merge_sort", "SortingNumbers");
+//		List<String> numbers3 = connectToSqlDB.readDataBase("merge_sort", "SortingNumbers");
+//		printValue(numbers);
 
 		//Quick Sort
 		algo.quickSort(num);
 		long quickSortExecutionTime = algo.executionTime;
 		System.out.println("Total Execution Time of "+ num.length + " numbers in Quick Sort take: " + quickSortExecutionTime + " milli sec");
-		connectToSqlDB.insertDataFromArrayToSqlTable(num, "quick_sort", "SortingNumbers");
-		List<String> numbers4 = connectToSqlDB.readDataBase("quick_sort", "SortingNumbers");
-		printValue(numbers);
-		int j = num.length;
-		randomize (num, j);
+//		connectToSqlDB.insertDataFromArrayToSqlTable(num, "quick_sort", "SortingNumbers");
+//		List<String> numbers4 = connectToSqlDB.readDataBase("quick_sort", "SortingNumbers");
+//		printValue(numbers);
+
 
 		//Heap Sort
 		algo.heapSort(num);
 		long heapSortExecutionTime = algo.executionTime;
 		System.out.println("Total Execution Time of "+ num.length + " numbers in Heap Sort take: " + heapSortExecutionTime + " milli sec");
-		connectToSqlDB.insertDataFromArrayToSqlTable(num, "heap_sort", "SortingNumbers");
-		List<String> numbers5 = connectToSqlDB.readDataBase("heap_sort", "SortingNumbers");
-		printValue(numbers);
-		int c = num.length;
-		randomize (num, c);
+//		connectToSqlDB.insertDataFromArrayToSqlTable(num, "heap_sort", "SortingNumbers");
+//		List<String> numbers5 = connectToSqlDB.readDataBase("heap_sort", "SortingNumbers");
+//		printValue(numbers);
+
 
 		//Bucket Sort
 		algo.bucketSort(num);
 		long bucketSortExecutionTime = algo.executionTime;
 		System.out.println("Total Execution Time of "+ num.length + " numbers in Bucket Sort take: " + bucketSortExecutionTime + " milli sec");
-		connectToSqlDB.insertDataFromArrayToSqlTable(num, "bucket_sort", "SortingNumbers");
-		List<String> numbers6 = connectToSqlDB.readDataBase("bucket_sort", "SortingNumbers");
-		printValue(numbers);
-		int k = num.length;
-		randomize (num, k);
+//		connectToSqlDB.insertDataFromArrayToSqlTable(num, "bucket_sort", "SortingNumbers");
+//		List<String> numbers6 = connectToSqlDB.readDataBase("bucket_sort", "SortingNumbers");
+//		printValue(numbers);
 
 		//Shell Sort
 		algo.shellSort(num);
 		long shellSortExecutionTime = algo.executionTime;
 		System.out.println("Total Execution Time of "+ num.length + " numbers in Shell Sort take: " + shellSortExecutionTime + " milli sec");
-		connectToSqlDB.insertDataFromArrayToSqlTable(num, "shell_sort", "SortingNumbers");
-		List<String> numbers7 = connectToSqlDB.readDataBase("shell_sort", "SortingNumbers");
-		printValue(numbers);
-		int x = num.length;
-		randomize (num, x);
+//		connectToSqlDB.insertDataFromArrayToSqlTable(num, "shell_sort", "SortingNumbers");
+//		List<String> numbers7 = connectToSqlDB.readDataBase("shell_sort", "SortingNumbers");
+//		printValue(numbers);
+
 
 		//Redix Sort
 		algo.redixSort(num);
 		long redixSortExecutionTime = algo.executionTime;
 		System.out.println("Total Execution Time of "+ num.length + " numbers in Redix Sort take: " + redixSortExecutionTime + " milli sec");
-		connectToSqlDB.insertDataFromArrayToSqlTable(num, "redix_sort", "SortingNumbers");
-		List<String> numbers8 = connectToSqlDB.readDataBase("redix_sort", "SortingNumbers");
-		printValue(numbers);
-		int y = num.length;
-		randomize (num, y);
-
-		//Come to conclusion about which Sorting Algo is better in given data set.
+//		connectToSqlDB.insertDataFromArrayToSqlTable(num, "redix_sort", "SortingNumbers");
+//		List<String> numbers8 = connectToSqlDB.readDataBase("redix_sort", "SortingNumbers");
+//		printValue(numbers);
 
 	}
+
+	//Come to conclusion about which Sorting Algo is better in given data set.
 
 	public static void storeRandomNumbers(int [] num){
 		Random rand = new Random();
 		for(int i=0; i<num.length; i++){
-			num[i] = rand.nextInt(1000000);
+			num[i] = rand.nextInt(100);
 		}
 	}
 
-	public static void randomize( int arr[], int a)
-	{
+	public static void randomize( int arr[], int a) {
 		Random r = new Random();
 		// Start from the last element and swap one by one. We don't
 		// need to run for the first element that's why i > 0
@@ -140,6 +129,8 @@ public class Numbers {
 			arr[j] = temp;
 		}
 	}
+
+
 	public static void printValue(List<String> array){
 		for(String st:array){
 			System.out.println(st);
--- src/algorithm/UnitTestSorting.java
+++ src/algorithm/UnitTestSorting.java
@@ -8,21 +8,70 @@ public class UnitTestSorting {
       This class is about Unit testing for Sorting Algorithm.
      */
     public static void main(String[] args) {
-        int [] unSortedArray = {6,9,2,5,1,0,4};
-        int [] sortedArray =   {0,1,2,4,5,6,9};
+        int[] unSortedArray = {6, 9, 2, 5, 1, 0, 4};
+        int[] sortedArray = {0, 1, 2, 4, 5, 6, 9};
         //Create Sort object
         Sort sort = new Sort();
         //apply unsorted array to selectionSort.
         sort.selectionSort(unSortedArray);
         //verify if the unsorted array is sorted by the selection sort algorithm.
         try {
-            Assert.assertEquals(sortedArray, unSortedArray, "Array is not Sorted");
-        }catch(Exception ex){
+            Assert.assertEquals(sortedArray, unSortedArray, "Selection: Array is not Sorted");
+        } catch (Exception ex) {
             ex.getMessage();
         }
 
         //Now implement Unit test for rest of the soring algorithm...................below
+        sort.insertionSort(unSortedArray);
+        try{
+            Assert.assertEquals(sortedArray, unSortedArray, "Insertion:  Array is not sorted");
+        }catch (Exception ex){
+            ex.getMessage();
+        }
 
+        sort.bubbleSort(unSortedArray);
+        try{
+            Assert.assertEquals(sortedArray,unSortedArray, "Bubble: Array is not sorted" );
+        }catch (Exception ex){
+            ex.getMessage();
+        }
 
+        sort.mergeSort(unSortedArray);
+        try{
+            Assert.assertEquals(sortedArray, unSortedArray, "Merge:  Array is not sorted");
+        }catch (Exception ex){
+            ex.getMessage();
+        }
+
+        sort.heapSort(unSortedArray);
+        try{
+            Assert.assertEquals(sortedArray, unSortedArray," Heap: Array is not sorted");
+        }catch (Exception ex) {
+            ex.getMessage();
+        }
+        sort.bucketSort(unSortedArray);
+        try{
+            Assert.assertEquals(sortedArray, unSortedArray, " Bucket: Array is not sorted");
+        }catch (Exception ex){
+            ex.getMessage();
+        }
+
+        sort.shellSort(unSortedArray);
+        try{
+            Assert.assertEquals(sortedArray, unSortedArray, " Shell: Array is not sorted");
+        }catch(Exception ex){
+            ex.getMessage();
+        }
+        sort.redixSort(unSortedArray);
+        try{
+            Assert.assertEquals(sortedArray, unSortedArray, " Redix: Array is not sorted");
+        }catch(Exception ex ){
+            ex.getMessage();
+        }
     }
 }
+
+
+
+
+
